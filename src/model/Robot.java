c1cbbcc5574075d9ede9ae04d5f70d97eb519ba4
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-11-13 19:05:49 -0600

    added variables

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -1,6 +1,21 @@
 
 public class Robot {
 
+	public int id;
+	public String name;
+	public int teamNumber;
+	public String jsonPath;
+	public String fourthPath;
+	//public Tile location;
+	public int matchesWon;
+	public int deathCount;
+	public int killCount;
+	public int health;
+	public int attack;
+	public int movement;
+	public int range;
+	public int distanceTraveled;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
