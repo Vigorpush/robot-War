f7ce9444d9f96922908d7947d2523f846cc55266
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-11-14 16:42:10 -0600

    some rearranging and added skeleton for only other function

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -1,6 +1,21 @@
 
 public class Robot {
 
+	public int id;
+	public int teamNumber;
+	public String jsonPath;
+	public String fourthPath;
+	public String name;
+	//public Tile location;
+	public int matchesWon;
+	public int deathCount;
+	public int killCount;
+	public int health;
+	public int attack;
+	public int movement;
+	public int range;
+	public int distanceTraveled;
+	
 	public Robot(int ID, int TEAM, String JPATH, String FPATH) {
 		id = ID;
 		teamNumber = TEAM;
@@ -18,21 +33,12 @@ public class Robot {
 		range = 0;
 		distanceTraveled = 0;
 	}
-
-	public int id;
-	public int teamNumber;
-	public String jsonPath;
-	public String fourthPath;
-	public String name;
-	//public Tile location;
-	public int matchesWon;
-	public int deathCount;
-	public int killCount;
-	public int health;
-	public int attack;
-	public int movement;
-	public int range;
-	public int distanceTraveled;
+	
+	//to be checked on and implemented outright
+	String[] interpreter(String[] s){
+		return s;
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
