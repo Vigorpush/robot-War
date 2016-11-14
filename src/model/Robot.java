5beb14f2c1b5dbca9d43a8d6da4be73b44d39aee
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-11-14 14:06:34 -0600

    added more to constructor

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -6,13 +6,24 @@ public class Robot {
 		teamNumber = TEAM;
 		jsonPath = JPATH;
 		fourthPath = FPATH;
+		
+		//name from json
+		//location = new Tile();
+		//matches won from json
+		//deathCount from json
+		//killcount from json
+		health = 0;
+		attack = 0;
+		movement = 0;
+		range = 0;
+		distanceTraveled = 0;
 	}
 
 	public int id;
-	public String name;
 	public int teamNumber;
 	public String jsonPath;
 	public String fourthPath;
+	public String name;
 	//public Tile location;
 	public int matchesWon;
 	public int deathCount;
