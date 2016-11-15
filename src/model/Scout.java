54353a23f599037755f4bc36975ce89b09107f21
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-11-14 19:02:46 -0600

    added scout class

---
+++ src/model/Scout.java
@@ -0,0 +1,14 @@
+
+public class Scout extends Robot {
+
+	public Scout(int ID, int TEAM, String JPATH, String FPATH) {
+		super(ID, TEAM, JPATH, FPATH);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
