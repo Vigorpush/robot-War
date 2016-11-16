d5332a8b922a11069f56b2e2e02ad631f8012b15
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-11-16 14:14:08 -0600

    updated constructor

--- src/model/Scout.java
+++ src/model/Scout.java
@@ -1,9 +1,13 @@
+package model;
 
 public class Scout extends Robot {
 
 	public Scout(int ID, int TEAM, String JPATH, String FPATH) {
 		super(ID, TEAM, JPATH, FPATH);
-		// TODO Auto-generated constructor stub
+		health = 1;
+		attack = 1;
+		movement = 3;
+		range = 2;
 	}
 
 	public static void main(String[] args) {
