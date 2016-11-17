e4c858c904e30259fc7f60b07c6b7b5e87e03079
kts135 <kts135@mail.usask.ca>
2016-11-17 16:47:58 -0600

    Added server classes

---
+++ src/model/Client.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Client {
+    // CONNECTION TO SERVER
+    // TODO: class connection to client
+    // TODO: Receiving thread
+    // TODO: sending thread
+    // TODO: input stream
+    // TODO: output stream
+    // TODO: client socket
+}
---
+++ src/model/Server.java
@@ -0,0 +1,21 @@
+package model;
+import java.util.*;
+
+public class Server {
+    // CONTAINERS
+    // TODO: list of player connections
+    // TODO: game state
+    
+    // NETWORKING STUFF
+    // TODO: server socket
+    // TODO: server thread
+    
+    // CONNECTION TO CLIENT
+    // TODO: class connection to client
+    // TODO: Receiving thread
+    // TODO: sending thread
+    // TODO: input stream
+    // TODO: output stream
+    // TODO: client socket
+    
+}
