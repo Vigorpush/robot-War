981a056271e02fae76a30fdfdb3cf63252270099
Brendon Sterma (bws948) <bws948@mail.usask.ca>
2016-12-03 18:40:48 -0600

    fourthvariable object for use with interpreter

---
+++ FourthVariable.java
@@ -0,0 +1,16 @@
+
+public class FourthVariable {
+
+	public String name;
+	public String value;
+	
+	public FourthVariable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
